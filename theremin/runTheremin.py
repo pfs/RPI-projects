db67725cc7173f22a113a07b169100017f0edd3e
pmvcfs <p.dro.ferreira.silva@gmail.com>
2021-05-16 17:39:54 +0200

    blocking requent sliding to  high pitch

--- theremin/runTheremin.py
+++ theremin/runTheremin.py
@@ -5,15 +5,23 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 
+_max_pitch=100
+
 def main():
     sensor = DistanceSensor(echo=17, trigger=4)
     sender = udp_client.SimpleUDPClient('127.0.0.1', 4559)
 
+    last_pitch=-1
+    
     while True:
 
         d=sensor.distance
         pitch = round(d * 100 + 30)
-        #print(d,pitch)
+        pitch = last_pitch if pitch>_max_pitch else pitch
+        last_pitch = pitch
+        print(last_pitch,pitch)
+        if pitch<0 :continue
+
         sender.send_message('/play_this', pitch)
         sleep(0.1)
 
